faa0c2b899b2a99bb11ba2340d3b26145d8ca03c
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-08-01 21:05:27 -0300

    Carregando arquivos CSS

--- conceitos-webpack/src/index.js
+++ conceitos-webpack/src/index.js
@@ -1,11 +1,12 @@
 import $ from 'jquery';
+import './css/style.css';
 
 $(function(){
 
     $('#btn').on('click', function(){
-
         $('h1').html("Olá mundo ALTERADO!");
 
+        $(this).addClass('botao');
     });
 
 });
